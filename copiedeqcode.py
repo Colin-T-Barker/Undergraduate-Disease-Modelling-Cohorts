b83c2199739ab843b654d9dcf29004c8d77a48e3
Kyle K <kylekeltnerdk@gmail.com>
2026-03-10 16:10:26 -0500

    less ugly, fully functional sliders, removed fluff

--- copiedeqcode.py
+++ copiedeqcode.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import scipy as sp
@@ -57,6 +57,7 @@ def odes(x: list, t: float) -> list:
     DEFINE PARAMTER VALUES
 '''
 
+
 # x0=[P_n, P_c, I, D_n, D_c]
 x_0 = [1.75e5, 40, 10, 0, 0] 
 
@@ -73,7 +74,7 @@ D_c = x[:,4]
 
 #Define Cancer
 C = P_c + D_c
-
+'''
 #Parameter vector
 params = np.array([lambda_p, lambda_c, lambda_i, Beta, alpha_c, alpha_i, S_pc, S_pn, S_i, r, n, x_0[0], x_0[1], x_0[2]])
 
@@ -90,6 +91,7 @@ scaled_sample = qmc.scale(sample, l_bounds, u_bounds)
 #print(f'Scaled LHS sample is {scaled_sample}')
 
 SSEs = np.zeros(len(scaled_sample))
+
 
 for i in range(0,len(scaled_sample)):
     lambda_p   = scaled_sample[i,0] 
@@ -109,6 +111,7 @@ for i in range(0,len(scaled_sample)):
     C_sim = np.interp(x_coords, t, C)
     ERRS = C_sim - np.array(y_coords)
     SSEs[i] = np.sum(ERRS**2)
+
 
 #Find the minimum SSE
 best_SSE = min(SSEs[0:i])
@@ -142,26 +145,11 @@ print(f"S_pn       = {best_S_pn}")
 print(f"S_i        = {best_S_i}")
 print(f"r          = {best_r}")
 print(f"n          = {best_n}")
+'''
 
-fig_plot, axs_plot = plt.subplots( 3, 2, figsize = ( 7, 7 ) )
-fig_sliders = plt.figure( figsize = ( 10, 5 ) )
+fig_plot, axs_plot = plt.subplots(3 , 2 , figsize = (7 , 7) )
+fig_sliders = plt.figure( figsize = ( 10 , 5 ) )
 
-# Container to hold strong references to sliders so they are not garbage collected
-fig_sliders.slider_refs = {}
-# additional module-level list for strong refs (helps avoid GC across backends)
-slider_refs_list = []
-
-def make_slider(fig, rect, label, valinit, valmin, valmax, refs, name=None):
-    ax = fig.add_axes(rect)
-    s = Slider(ax=ax, label=label, valmin=valmin, valmax=valmax, valinit=valinit)
-    # store strong reference
-    if name:
-        refs[name] = s
-    else:
-        refs.setdefault('list', []).append(s)
-    # also keep module-level strong reference
-    slider_refs_list.append(s)
-    return s
 
 '''
     PLOTS FOR 5 PARAMETERS AND TOTAL CANCER CELLS
@@ -176,11 +164,6 @@ plots = [
     (axs_plot[2, 1], C,   'red',  '$C(t)$', 'Total Cancer Cells'),
 ]
 
-'''
-    SLIDERS FOR COEFFICIENTS
-'''
-coefficients = [lambda_p, lambda_c, lambda_i, Beta, alpha_c, alpha_i, S_pc, S_pn, S_i, r, n]
-
 for ax, data, color, title, ylabel in plots:
     ax.plot(t, data, color = color)
     ax.set_title(title)
@@ -192,38 +175,30 @@ for ax, data, color, title, ylabel in plots:
 '''
     SLIDERS FOR COEFFICIENT ADJUSTMENT
 '''
-beta_slider = make_slider(fig_sliders, [0.06, 0.9, 0.35, 0.03], 'Beta', best_Beta, best_Beta - best_Beta * uni_scaler, best_Beta + best_Beta * uni_scaler, fig_sliders.slider_refs, name='beta_slider')
-lambda_p_slider = make_slider(fig_sliders, [0.56, 0.9, 0.35, 0.03], 'Lambda_p', best_lambda_p, best_lambda_p - best_lambda_p * uni_scaler, best_lambda_p + best_lambda_p * uni_scaler, fig_sliders.slider_refs, name='lambda_p_slider')
-lambda_c_slider = make_slider(fig_sliders, [0.06, 0.85, 0.35, 0.03], 'Lambda_c', best_lambda_c, best_lambda_c - best_lambda_c * uni_scaler, best_lambda_c + best_lambda_c * uni_scaler, fig_sliders.slider_refs, name='lambda_c_slider')
-lambda_i_slider = make_slider(fig_sliders, [0.56, 0.85, 0.35, 0.03], 'Lambda_i', best_lambda_i, best_lambda_i - best_lambda_i * uni_scaler, best_lambda_i + best_lambda_i * uni_scaler, fig_sliders.slider_refs, name='lambda_i_slider')
-alpha_c_slider = make_slider(fig_sliders, [0.06, 0.8, 0.35, 0.03], 'Alpha_c', best_alpha_c, best_alpha_c - best_alpha_c * uni_scaler, best_alpha_c + best_alpha_c * uni_scaler, fig_sliders.slider_refs, name='alpha_c_slider')
-alpha_i_slider = make_slider(fig_sliders, [0.56, 0.8, 0.35, 0.03], 'Alpha_i', best_alpha_i, best_alpha_i - best_alpha_i * uni_scaler, best_alpha_i + best_alpha_i * uni_scaler, fig_sliders.slider_refs, name='alpha_i_slider')
-S_pc_slider = make_slider(fig_sliders, [0.06, 0.75, 0.35, 0.03], 'S_pc', best_S_pc, best_S_pc - best_S_pc * uni_scaler, best_S_pc + best_S_pc * uni_scaler, fig_sliders.slider_refs, name='S_pc_slider')
-S_pn_slider = make_slider(fig_sliders, [0.56, 0.75, 0.35, 0.03], 'S_pn', best_S_pn, best_S_pn - best_S_pn * uni_scaler, best_S_pn + best_S_pn * uni_scaler, fig_sliders.slider_refs, name='S_pn_slider')
-S_i_slider = make_slider(fig_sliders, [0.06, 0.7, 0.35, 0.03], 'S_i', best_S_i, best_S_i - best_S_i * uni_scaler, best_S_i + best_S_i * uni_scaler, fig_sliders.slider_refs, name='S_i_slider')
-r_slider = make_slider(fig_sliders, [0.56, 0.7, 0.35, 0.03], 'r', best_r, best_r - best_r * uni_scaler, best_r + best_r * uni_scaler, fig_sliders.slider_refs, name='r_slider')
-n_slider = make_slider(fig_sliders, [0.06, 0.65, 0.35, 0.03], 'n', best_n, best_n - best_n * uni_scaler, best_n + best_n * uni_scaler, fig_sliders.slider_refs, name='n_slider')
 
-sliders = [fig_sliders.slider_refs[k] for k in ['beta_slider','lambda_p_slider','lambda_c_slider','lambda_i_slider','alpha_c_slider','alpha_i_slider','S_pc_slider','S_pn_slider','S_i_slider','r_slider','n_slider']]
+beta_slider = Slider(fig_sliders.add_axes([0.06, 0.9, 0.35, 0.03]), label = 'Beta', valmin = 0, valmax = 0.0001, valstep = 0.00000001, valinit = 0 )
+lambda_p_slider = Slider(fig_sliders.add_axes([0.06, 0.8, 0.35, 0.03]), label = 'lambda_p', valmin = 0, valmax = 20, valstep = 0.1, valinit = lambda_p )
+lambda_c_slider = Slider(fig_sliders.add_axes([0.06, 0.7, 0.35, 0.03]), label = 'lambda_c', valmin = 0, valmax = 20, valstep = 0.1, valinit = lambda_c )
+lambda_i_slider = Slider(fig_sliders.add_axes([0.06, 0.6, 0.35, 0.03]), label = 'lambda_i', valmin = 0, valmax = 20, valstep = 0.1, valinit = lambda_i )
+alpha_c_slider = Slider(fig_sliders.add_axes([0.06, 0.5, 0.35, 0.03]), label = 'alpha_c', valmin = 0, valmax = 1, valstep = 0.01, valinit = alpha_c )
+alpha_i_slider = Slider(fig_sliders.add_axes([0.56, 0.9, 0.35, 0.03]), label = 'alpha_i', valmin = 0, valmax = 1, valstep = 0.01, valinit = alpha_i )
+S_pc_slider = Slider(fig_sliders.add_axes([0.56, 0.8, 0.35, 0.03]), label = 'S_pc', valmin = 0, valmax = 1, valstep = 0.01, valinit = S_pc )
+S_pn_slider = Slider(fig_sliders.add_axes([0.56, 0.7, 0.35, 0.03]), label = 'S_pn', valmin = 0, valmax = 1e-5, valstep = 1e-7, valinit = S_pn )
+S_i_slider = Slider(fig_sliders.add_axes([0.56, 0.6, 0.35, 0.03]), label = 'S_i', valmin = 0, valmax = 10, valstep = 0.1, valinit = S_i )
+r_slider = Slider(fig_sliders.add_axes([0.56, 0.5, 0.35, 0.03]), label = 'r', valmin = 0, valmax = 5, valstep = 0.1, valinit = r )
+n_slider = Slider(fig_sliders.add_axes([0.56, 0.4, 0.35, 0.03]), label = 'n', valmin = 0, valmax = 1, valstep = 0.01, valinit = n )
+
+sliders = [ beta_slider, lambda_p_slider, lambda_c_slider, lambda_i_slider, alpha_c_slider, alpha_i_slider, S_pc_slider, S_pn_slider, S_i_slider, r_slider, n_slider ]
 
 def update(val):
-    current_beta = beta_slider.val
-    current_lambda_p = lambda_p_slider.val
-    current_lambda_c = lambda_c_slider.val
-    current_lambda_i = lambda_i_slider.val
-    current_alpha_c = alpha_c_slider.val
-    current_alpha_i = alpha_i_slider.val
-    current_S_pc = S_pc_slider.val
-    current_S_pn = S_pn_slider.val
-    current_S_i = S_i_slider.val
-    current_r = r_slider.val
-    current_n = n_slider.val
+    for i in sliders:
+        current_[i] = i.val
 
-    dP_ndt = current_lambda_p - current_beta * I * P_n - current_S_pn * P_n
-    dP_cdt = current_lambda_c * P_c + current_beta * I * P_n - current_alpha_i * I * P_c - current_S_pc * P_c
-    dIdt   = current_lambda_i + current_alpha_c * P_c - current_S_i * I
-    dD_ndt = current_S_pn * P_n - current_r * D_n
-    dD_cdt = current_S_pc * P_c + current_alpha_i * I * P_c - current_n * current_r*I * D_c
+    dP_ndt = lambda_p - current_beta * I * P_n - S_pn * P_n
+    dP_cdt = lambda_c * P_c + current_beta * I * P_n - alpha_i * I * P_c - S_pc * P_c
+    dIdt   = lambda_i + alpha_c * P_c - S_i * I
+    dD_ndt = S_pn * P_n - r * D_n
+    dD_cdt = S_pc * P_c + alpha_i * I * P_c - n * r*I * D_c
     
     axs_plot[0, 0].lines[0].set_ydata(dP_ndt)
     axs_plot[0, 1].lines[0].set_ydata(dP_cdt)
@@ -232,25 +207,11 @@ def update(val):
     axs_plot[2, 0].lines[0].set_ydata(dIdt)
 
     fig_plot.canvas.draw_idle()
-    # also redraw slider figure to keep widgets responsive across backends
-    try:
-        fig_sliders.canvas.draw_idle()
-    except Exception:
-        pass
 
-for s in sliders:
-    s.on_changed(update)
 
 reset_axs = fig_sliders.add_axes([0.8, 0.05, 0.1, 0.04])
 button = matplotlib.widgets.Button(reset_axs, 'Reset', color='lightgray', hovercolor='gray')
 
-def reset(event):
-    for s in sliders:
-        try:
-            s.reset()
-        except Exception:
-            pass
-    
-button.on_clicked(reset)
+beta_slider.on_changed(update)
 
 plt.show()
